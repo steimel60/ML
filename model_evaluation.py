dfe2044a5a49d7c2148dbfc03a13b58be7ca7c48
steimel60 <72476768+steimel60@users.noreply.github.com>
2022-05-04 17:41:56 -0400

    Grid Search Examples
    Examples adjusting and analyzing parameters using grid search

--- model_evaluation.py
+++ model_evaluation.py
@@ -58,3 +58,81 @@ print(f"Non-Stratified Split k=3 on Classification Data: {scores}")
 kfold = KFold(n_splits=3, shuffle=True, random_state=0) #shuffled splitter
 scores = cross_val_score(logreg, iris.data, iris.target, cv=kfold)
 print(f"Shuffled Split k=3 on Classification Data: {scores}")
+
+
+
+#------------------------- Grid Search --------------------------------------#
+from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GridSearchCV
+
+"""
+Grid Search loops over possible parameter options to find the best combination.
+For example, consider the C and gamma parameters in the SVC class.
+If we want to check C=.001 and .1 and gamma = .001 and 100 we get the following grid
+                C=.001                      C=.1
+gamma=.001      SVC(C=.001, gamma=.001)     SVC(C=.1, gamma=.001)
+gamma=100       SVC(C=.001, gamma=100)      SVC(C=.1, gamma=100)
+"""
+
+#If we use our test set to adjust parameters we can no longer trust that it's not overfitting
+#We can test on a "validation set" to fix this
+mglearn.plots.plot_threefold_split()
+#plt.show()
+
+#Split training and validation data from our test set
+X_trainval, X_test, y_trainval, y_test = train_test_split(iris.data, iris.target, random_state=0)
+#Further split our training and validation data
+X_train, X_val, y_train, y_val = train_test_split(X_trainval, y_trainval, random_state=1)
+
+#Grid search
+best_score=0
+for gamma in [.001, .01, .1, 1, 10, 100]:
+    for C in [.001, .01, .1, 1, 10, 100]:
+        svm = SVC(gamma=gamma, C=C)
+        svm.fit(X_train, y_train)
+        score = svm.score(X_val, y_val)
+        if score > best_score:
+            best_score = score
+            best_parameters = {'C':C, 'gamma':gamma}
+svm = SVC(**best_parameters).fit(X_trainval, y_trainval)
+test_score = svm.score(X_test, y_test)
+print(f"Best score on Validation set: {best_score}")
+print(f"Best parameters: {best_parameters}")
+print(f"Test Score with best params: {test_score}")
+
+"""
+We can get a better idea of the generalization performance by combining grid search and cross validation.
+"""
+best_score=0
+for gamma in [.001, .01, .1, 1, 10, 100]:
+    for C in [.001, .01, .1, 1, 10, 100]:
+        svm = SVC(gamma=gamma, C=C)
+        scores = cross_val_score(svm, X_trainval, y_trainval, cv=5)
+        score = np.mean(scores)
+        if score > best_score:
+            best_score = score
+            best_parameters = {'C':C, 'gamma':gamma}
+svm = SVC(**best_parameters).fit(X_trainval, y_trainval)
+test_score = svm.score(X_test, y_test)
+print(f"Best score on Validation set: {best_score}")
+print(f"Best parameters: {best_parameters}")
+print(f"Test Score with best params: {test_score}")
+
+
+"""
+Because this is common practice sklearn has a class to help with the process
+"""
+param_grid = {'C':[.001, .01, .1, 1, 10, 100],'gamma':[.001, .01, .1, 1, 10, 100]} #Dict with parameters we want to check
+grid_search = GridSearchCV(SVC(), param_grid, cv=5, return_train_score=True)
+X_train, X_test, y_train, y_test = train_test_split(iris.data, iris.target, random_state=0)
+
+grid_search.fit(X_train, y_train)
+print(f"GridSearchCV Score: {grid_search.score(X_test, y_test)}")
+print(f"GSCV Best Params: {grid_search.best_params_}")
+print(f"GSCV Best Score: {grid_search.best_score_}")
+print(f"GSCV Best Estimator: {grid_search.best_estimator_}")
+
+#It can be useful to see the results of each iteration
+results = pd.DataFrame(grid_search.cv_results_)
+print(results.head())
